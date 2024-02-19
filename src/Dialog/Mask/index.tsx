6e488f53620dae868776bef306d5dcf0e47baaed
tangxiaoxin <1302947749@qq.com>
2024-02-19 15:56:32 +0800

    📦0.0.4: stopPropagation

--- src/Dialog/Mask/index.tsx
+++ src/Dialog/Mask/index.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
 import styles from "./index.module.less";
 const Mask = ({ children }: { children?: React.ReactNode }) => {
-  return <div className={styles.mask}>{children}</div>;
+  return (
+    <div
+      className={styles.mask}
+      onClick={(e) => {
+        e.stopPropagation();
+      }}
+    >
+      {children}
+    </div>
+  );
 };
 
-export default Mask
+export default Mask;
